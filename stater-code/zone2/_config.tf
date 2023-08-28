974c4d8d7c7676dc6a874a782aed15f4f7f3c2a1
nhadd <aoducnha29493@gmail.com>
2023-08-29 00:02:25 +0700

    u

--- stater-code/zone2/_config.tf
+++ stater-code/zone2/_config.tf
@@ -1,14 +1,13 @@
 terraform {
    backend "s3" {
-     bucket = "project2-066778212893-nhadd-west"
+     bucket = "udacity-tf-nhadd-west"
      key    = "terraform/terraform.tfstate"
      region = "us-west-1"
    }
  }
 
  provider "aws" {
-    region = "us-west-1"
-   #profile = "default"
+   region = "us-west-1"
    
    default_tags {
      tags = local.tags
